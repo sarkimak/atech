fa14169663d245a8f4fe6619ae5adf9776325107
Sarah Kimak <sarahekimak@gmail.com>
2022-03-22 15:02:24 -0400

    welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                Welcome to UD CISC275 Agile Activity
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
