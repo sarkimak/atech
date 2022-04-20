135fcd65c8584b3b007d4f6991db2c6a7f769cf4
Sarah Kimak <sarahekimak@gmail.com>
2022-04-20 11:27:16 -0400

    trying this again

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Welcome to UD CISC275 Agile Activity - Lexi and Sarah
+                Assistive Technology Progress
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <Button id="ex-button" onClick={() => console.log("clicked")}>
+                Button!
+            </Button>
         </div>
     );
 }
